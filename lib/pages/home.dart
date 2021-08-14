c7877c923198d7e1a220efadd5b6be436ca5ac13
Bkushagra <88078180+Bkushagra@users.noreply.github.com>
2021-08-14 08:15:43 +0530

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,18 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'homepage',
-      home: Home(),
-    );
-
-  }
-}
 
 class Home extends StatefulWidget {
   @override
